d6dc259f641e01013aaca7b30a59e2cdb1bd4411
ImNotTwig <ImNotTwig@gmail.com>
2023-06-08 13:22:36 -0500

    main.go and utility.go

--- go.mod
+++ go.mod
@@ -1,3 +1,11 @@
-module UnboundHelper_v4
+module unbound_helper_v_something
 
 go 1.20
+
+require (
+	github.com/diamondburned/arikawa/v3 v3.3.0 // indirect
+	github.com/gorilla/schema v1.2.0 // indirect
+	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
+)
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/diamondburned/arikawa/v3/gateway"
+	"github.com/diamondburned/arikawa/v3/session"
+)
+
+var (
+	token  = "MTA0NDEyMjE3MDExODM4NTc3NQ.GgMhci.cMzKZog3m9vpEBfBBdzFmCsUBS04OqK4MqvW8M"
+	prefix = ";"
+)
+
+func main() {
+	// Getting and checking if the token is empty
+	if strings.TrimSpace(token) == "" {
+		log.Fatalln("No Token Provided.")
+	}
+	// Creating a new session.
+	s := session.New("Bot " + token)
+	// Adding a handler to our session that checks whenever a message is sent and prints the username of the sender
+	// and the content of the message
+	s.AddHandler(handle_message)
+	// Adding Intents
+	s.AddIntents(gateway.IntentGuildMessages)
+	s.AddIntents(gateway.IntentDirectMessages)
+	// Connecting to Discord
+	if err := s.Open(context.Background()); err != nil {
+		log.Fatalln("Failed to connect:", err)
+	}
+	defer s.Close()
+
+	// Getting the bot's user data
+	me, err := s.Me()
+	if err != nil {
+		log.Fatalln("Failed to get myself:", err)
+	}
+
+	log.Println("Started as", me.Username)
+
+	// Block forever, so the program doesnt close.
+	select {}
+}
+
+// checks if a message is a command, if not it returns, if so parse the command
+func handle_message(c *gateway.MessageCreateEvent) {
+	message := strings.ToLower(c.Content)
+	// checking if the message is prefixed with the bot prefix
+	message, found := strings.CutPrefix(message, prefix)
+	if !found {
+		return
+	}
+	// Splitting all the words in the message, to determine the command and args
+	words_in_message := strings.Fields(message)
+	// Checking the command
+	command := words_in_message[0]
+	args := words_in_message[1:]
+	// TODO: check if command exists
+	fmt.Println(command, " ", args)
+}
